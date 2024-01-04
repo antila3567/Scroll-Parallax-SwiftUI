b1a627cc8496410d7d9eb20719a8324d98731353
IvanL <antila3567@gmail.com>
2024-01-04 10:44:43 +0200

    initial commit

---
+++ ScrollParalax/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  ScrollParalax
+//
+//  Created by Иван Легенький on 04.01.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
