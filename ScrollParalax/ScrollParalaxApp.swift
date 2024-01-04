f1c517d8003ae943bd10227c8c0c9ab2e6718dc9
IvanL <antila3567@gmail.com>
2024-01-04 10:16:44 +0200

    Initial Commit

---
+++ ScrollParalax/ScrollParalaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollParalaxApp.swift
+//  ScrollParalax
+//
+//  Created by Иван Легенький on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrollParalaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
